5c8f2c5f77d401471dee8a4c43bec08a018fd229
Pascal Schärli <pas.schaerli@sunrise.ch>
2020-12-28 14:46:43 +0100

    Finished arguments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from utils import parse_args
+from facechanger.utils import parse_args
 
 if __name__ == "__main__":
-    parse_args()
+    args = parse_args()
+    print(args)
